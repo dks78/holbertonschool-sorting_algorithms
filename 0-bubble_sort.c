32e6f7314486324abe179503cda92cd6015bd761
aurelien <aurelienmest@outlook.fr>
2025-01-07 12:30:05 +0100

    chal8

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,31 +9,30 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+size_t i, j;
+int temp;
+int swapped;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swapped = 1;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-
-                /* Call to the provided print_array function */
-                print_array(array, size);
-            }
-        }
-        if (!swapped)
-        break;
-    }
+/* Call to the provided print_array function */
+print_array(array, size);
+}
+}
+if (!swapped)
+break;
+}
 }
 
